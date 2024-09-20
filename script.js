23ab7061e7467a7c28d69f6fc0068d6c72e666c4
JmzTradehacker <japhet.mdn@gmail.com>
2024-09-19 20:16:40 -0700

    added 2d array

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-let rows = csvString.split('\n');
-let newArray = []
+const rows = csvString.split('\n');
+const header = rows[0].split(',');
+const numcol = header.length;
+
+let newArray = [];
 
 for (let i = 0; i < rows.length; i++) {
   let col = rows[i].split(',');
